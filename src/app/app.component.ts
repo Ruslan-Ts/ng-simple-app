96b63ea84a163740574a645aa18758906bf5f558
Ruslan Tsuman <r.ts@me.com>
2024-04-02 18:20:03 +0200

    shopping-list_add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+
+class Item {
+  name: string;
+  price: number;
+  done: boolean;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+    this.done = false;
+  }
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng-simple-app';
+  title = 'Shopping list';
+
+  text: string = '';
+  price: number = 0;
+
+  items: Item[] = [
+    {name: 'apple', price: 12, done: false},
+    {name: 'orange', price: 14, done: false},
+    {name: 'fish', price: 12, done: false},
+    {name: 'meat', price: 11, done: false},
+    {name: 'water', price: 5, done: true},
+  ]
+
+  addItem(text: string, price: number): void {
+    if(text === null || text.trim() === "" || price === null)
+            return;
+        this.items.push(new Item(text, price));
+  }
 }
