e07fa701ec5176460b6eaaaf6f67a4825d020dac
Ruslan Tsuman <r.ts@me.com>
2024-04-02 12:34:11 +0200

    test-component

--- src/app/componets/movie/movie.component.ts
+++ src/app/componets/movie/movie.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-movie',
+     standalone: true,
     templateUrl: './movie.component.html'
 })
 export class MovieComponent {
